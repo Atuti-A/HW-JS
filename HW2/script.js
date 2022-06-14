efa515a6fba617b8b0bb4c8fa9f23bfb509d690f
Atuti-A <10charlie39@gmail.com>
2022-06-14 16:09:42 +0300

    Add HW 3

--- HW2/script.js
+++ HW2/script.js
@@ -5,44 +5,66 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-class GoodsItem {
-    constructor({ title = '', price = 0 }) {
-        this.title = title;
-        this.price = price;
-    }
+const GET_GOODS_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
+const GET_BASKET_GOODS_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json'
 
-    render() {
-        return `
-    <div class="goods-item">
-    <h3>${this.title}</h3>
-    <p>${this.price}</p>
-    </div>    
-    `
+function service(url, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onload = () => {
+        callback(JSON.parse(xhr.response))
     }
 }
 
-
+class GoodsItem {
+    constructor({ product_name, price }) {
+        this.product_name = product_name;
+        this.price = price;
+    }
+    render() {
+        return `
+      <div class="goods-item">
+        <h3>${this.product_name}</h3>
+        <p>${this.price}</p>
+      </div>
+    `;
+    }
+}
 class GoodsList {
-    item = [];
-    fetchGoods() {
-        this.list = goods;
+    items = [];
+    fetchGoods(callback) {
+        service(GET_GOODS_ITEMS, (data) => {
+            this.items = data;
+            callback()
+        });
     }
     calculatePrice() {
-        return this.item.reduce((prev, { price }) => {
+        return this.items.reduce((prev, { price }) => {
             return prev + price;
         }, 0)
     }
     render() {
-        const goodsList = this.list.map(item => {
-            const goodsItem = new GoodsItem(item);
-            return goodsItem.render()
+        const goods = this.items.map(item => {
+            const goodItem = new GoodsItem(item);
+            return goodItem.render()
         }).join('');
 
-        document.querySelector('.goods-list').innerHTML = goodsList;
+        document.querySelector('.goods-list').innerHTML = goods;
     }
 }
 
-const goodsList = new GoodsList(goods);
-goodsList.fetchGoods()
-goodsList.render()
-console.log(goodsList.calculatePrice())
+class BasketGoodsList {
+    items = [];
+    fetchGoods(callback) {
+        service(GET_BASKET_GOODS_ITEMS, (data) => {
+            this.items = data.contents;
+            callback()
+        });
+    }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods(() => {
+    goodsList.render();
+});
